8340522a9b5225818acf14d762854690ffc26566
MashoodBilal <44563181+MashoodBilal@users.noreply.github.com>
2024-05-05 00:07:42 +0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,10 +10,9 @@ if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
     Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
 }
 Import-Module -Name Terminal-Icons
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
-}
+Import-Module -Name z
+Import-Module PSReadLine
+
 
 # Check for Profile Updates
 function Update-Profile {
@@ -85,31 +84,128 @@ function Test-CommandExists {
     return $exists
 }
 
-# Editor Configuration
-$EDITOR = if (Test-CommandExists nvim) { 'nvim' }
-          elseif (Test-CommandExists pvim) { 'pvim' }
-          elseif (Test-CommandExists vim) { 'vim' }
-          elseif (Test-CommandExists vi) { 'vi' }
-          elseif (Test-CommandExists code) { 'code' }
-          elseif (Test-CommandExists notepad++) { 'notepad++' }
-          elseif (Test-CommandExists sublime_text) { 'sublime_text' }
-          else { 'notepad' }
-Set-Alias -Name vim -Value $EDITOR
 
-function Edit-Profile {
-    vim $PROFILE.CurrentUserAllHosts
+# for PSReadLine
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -EditMode Emacs
+
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If so and the current host is a command line, then change to red color 
+# as warning to user that they are operating in an elevated context
+# Useful shortcuts for traversing directories
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Quick shortcut to start notepad
+function n { notepad++ $args }
+
+# Drive shortcuts
+function HKLM: { Set-Location HKLM: }
+function HKCU: { Set-Location HKCU: }
+function Env: { Set-Location Env: }
+
+# Creates drive shortcut for Work Folders, if current user account is using it
+if (Test-Path "$env:USERPROFILE\Work Folders") {
+    New-PSDrive -Name Work -PSProvider FileSystem -Root "$env:USERPROFILE\Work Folders" -Description "Work Folders"
+    function Work: { Set-Location Work: }
 }
-function touch($file) { "" | Out-File $file -Encoding ASCII }
-function ff($name) {
-    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
-        Write-Output "$($_.directory)\$($_)"
+
+# Set up command prompt and window title. Use UNIX-style convention for identifying 
+# whether user is elevated (root) or not. Window title shows current version of PowerShell
+# and appends [ADMIN] if appropriate for easy taskbar identification
+function prompt { 
+    if ($isAdmin) {
+        "[" + (Get-Location) + "] # " 
+    } else {
+        "[" + (Get-Location) + "] $ "
     }
 }
 
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
+if ($isAdmin) {
+    $Host.UI.RawUI.WindowTitle += " [ADMIN]"
+}
+
+# Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
+function dirs {
+    if ($args.Count -gt 0) {
+        Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
+    } else {
+        Get-ChildItem -Recurse | Foreach-Object FullName
+    }
+}
+
+# Simple function to start a new elevated process. If arguments are supplied then 
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin {
+	Start-Process pwsh -Verb RunAs
+}
+
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+Set-Alias -name 'npp' -value 'C:\Program Files\Notepad++\notepad++.exe'
+Set-Alias -name 'notepad++' -value 'C:\Program Files\Notepad++\notepad++.exe'
+
+
+# Make it easy to edit this profile once it's installed
+function Edit-Profile {
+    vim $PROFILE.CurrentUserAllHosts
+}
+
+# We don't need these any more; they were just temporary variables to get to $isAdmin. 
+# Delete them to prevent cluttering up the user profile. 
+Remove-Variable identity
+Remove-Variable principal
+
+Function Test-CommandExists {
+    Param ($command)
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'SilentlyContinue'
+    try { if (Get-Command $command) { RETURN $true } }
+    Catch { Write-Host "$command does not exist"; RETURN $false }
+    Finally { $ErrorActionPreference = $oldPreference }
+}
 # Network Utilities
 function Get-PubIP { (Invoke-WebRequest http://ifconfig.me/ip).Content }
 
-# System Utilities
+# Aliases
+#
+# If your favorite editor is not here, add an elseif and ensure that the directory it is installed in exists in your $env:Path
+#
+if (Test-CommandExists nvim) {
+    $EDITOR='nvim'
+} elseif (Test-CommandExists pvim) {
+    $EDITOR='pvim'
+} elseif (Test-CommandExists vim) {
+    $EDITOR='vim'
+} elseif (Test-CommandExists vi) {
+    $EDITOR='vi'
+} elseif (Test-CommandExists code) {
+    $EDITOR='code'
+} elseif (Test-CommandExists notepad) {
+    $EDITOR='notepad'
+} elseif (Test-CommandExists notepad++) {
+    $EDITOR='npp'
+} 
+Set-Alias -Name vim -Value $EDITOR
+
+
+function ls { Get-ChildItem $pwd -File }
+function ll { Get-ChildItem $pwd  }
 function uptime {
     if ($PSVersionTable.PSVersion.Major -eq 5) {
         Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
@@ -119,9 +215,20 @@ function uptime {
 }
 
 function reload-profile {
-    & $profile
+     $profile
+     & $profile
+     & $Profile
+     & $PROFILE
+     . $profile
+     . $Profile
+     . $PROFILE
 }
-
+function find-file($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
+}
 function unzip ($file) {
     Write-Output("Extracting", $file, "to", $pwd)
     $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
@@ -159,27 +266,24 @@ function grep($regex, $dir) {
     }
     $input | select-string $regex
 }
-
+function touch($file) {
+    "" | Out-File $file -Encoding ASCII
+}
 function df {
     get-volume
 }
-
 function sed($file, $find, $replace) {
     (Get-Content $file).replace("$find", $replace) | Set-Content $file
 }
-
 function which($name) {
     Get-Command $name | Select-Object -ExpandProperty Definition
 }
-
 function export($name, $value) {
     set-item -force -path "env:$name" -value $value;
 }
-
 function pkill($name) {
     Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
 }
-
 function pgrep($name) {
     Get-Process $name
 }
@@ -257,7 +361,7 @@ Set-PSReadLineOption -Colors @{
 }
 
 ## Final Line to set prompt
-oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json | Invoke-Expression
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
 } else {
